e726261d60fd197544ebfb963bc94fd2af562bd3
Bruno <brun0yuj1ym02@gmail.com>
2023-01-13 13:28:19 +0900

    update logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { HeaderContainer, InformationContainer, Local } from './styles';
-import logo from '../../../public/logo.png';
+import logo from '../../assets/Logo.png';
 import { MapPin, ShoppingCart } from 'phosphor-react';
 import { Link } from 'react-router-dom';
 import { useCoffee } from '../../context/CoffeeContext';
